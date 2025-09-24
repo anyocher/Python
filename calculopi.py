e2d3369c9cc82690a69c74dbcee899861eca627a
Ochner <140626175+anyocher@users.noreply.github.com>
2025-09-24 14:03:29 -0300

    Create calculopi.py

---
+++ calculopi.py
@@ -0,0 +1,14 @@
+# b-) riar um programa em Python que calcule o raio de uma circunferência.
+
+import math  # para usar o valor de pi
+
+print("-----Cálculo do Raio da Circunferência-----\n")
+
+# Entrada do usuário
+circunferencia = float(input("Digite o valor da circunferência (C): "))
+
+# Cálculo do raio
+raio = circunferencia / (2 * math.pi)
+
+# Saída formatada
+print(f"O raio da circunferência é: {raio:.2f}")
