c642c5156a876b411345fd17e62b24dd87500cfe
anyocher <anaochnerort@gmail.com>
2025-10-07 20:39:29 -0300

    Repository update

--- Aula 01/calculopi.py
+++ Aula 01/calculopi.py
@@ -1,6 +1,7 @@
 # b-) riar um programa em Python que calcule o raio de uma circunferência.
 
-import math  # para usar o valor de pi
+#import math  # para usar o valor de pi
+import math as m 
 
 print("-----Cálculo do Raio da Circunferência-----\n")
 
@@ -8,7 +9,7 @@ print("-----Cálculo do Raio da Circunferência-----\n")
 circunferencia = float(input("Digite o valor da circunferência (C): "))
 
 # Cálculo do raio
-raio = circunferencia / (2 * math.pi)
+raio = circunferencia / (2 * m.pi)
 
 # Saída formatada
 print(f"O raio da circunferência é: {raio:.2f}")
---
+++ Aula 04/exemplofuncao01.py
@@ -0,0 +1,15 @@
+def somar_dados(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+def mostrar_dados(resultado):
+    print("O valor da soma é:", resultado)
+
+
+
+if __name__ == "__main__":
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+
+processa = somar_dados(num1, num2)
+mostrar = mostrar_dados(processa)
