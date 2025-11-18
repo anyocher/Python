ec441904224841abae1dc8304cba699137c591c2
anyocher <anaochnerort@gmail.com>
2025-11-18 20:40:15 -0300

    Update New File

--- Dashboard/Atividade Avaliativa Inventario/PYTH_07.py
+++ Dashboard/Atividade Avaliativa Inventario/PYTH_07.py
@@ -1,49 +1,41 @@
 import pandas as pd
 
 def carregar_dados(url):
-   
     return pd.read_csv(url, sep=";")
 
 def calcular_totais(df):
- 
     df["PurchasePriceTotal"] = df["PurchasePrice"] * df["StockQuantity"]
     df["SalePriceTotal"] = df["SalePrice"] * df["StockQuantity"]
     return df
 
+
 def salvar_arquivos(df, prefixo="inventario"):
-  
-    df.to_csv(f"{prefixo}_totais.csv", index=False)
-    df.to_excel(f"{prefixo}_totais.xlsx", index=False)
+    df.to_csv(f"{prefixo}.csv", index=False)
+    # df.to_excel(f"{prefixo}.xlsx", index=False)
 
 def gerar_relatorio_fornecedores(df):
-    
     df_fornecedor = df.groupby("Supplier").sum(numeric_only=True).reset_index()
     df_fornecedor = df_fornecedor.sort_values("Supplier")
     return df_fornecedor
 
 def filtrar_acima60(df):
-   
     return df[df["StockQuantity"] > 60]
 
 def main(url):
-  
     df = carregar_dados(url)
-    
 
     df = calcular_totais(df)
-    
-    salvar_arquivos(df)
-    
+    salvar_arquivos(df, prefixo="inventario_totais")
+
     df_fornecedor = gerar_relatorio_fornecedores(df)
     salvar_arquivos(df_fornecedor, prefixo="inventario_fornecedores")
-    
+
     df_acima60 = filtrar_acima60(df)
     salvar_arquivos(df_acima60, prefixo="inventario_acima60")
-    
+
     print("Arquivos gerados...")
 
 # URL do arquivo CSV
-# url = "https://raw.githubusercontent.com/JansenLeite/Python/main/PYTH_07_01_Inventario.csv"
+url = "https://raw.githubusercontent.com/JansenLeite/Python/main/PYTH_07_01_Inventario.csv"
 
-# main(url)
-
+main(url)
